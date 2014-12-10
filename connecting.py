7bef6690a74c6863476c778db5eff8ed77c942dc
Lauri Hakko <lauri.hakko@gmail.com>
2014-12-10 16:21:31 +0200

    fixed the ball color

--- connecting.py
+++ connecting.py
@@ -25,7 +25,7 @@ class Animation(Widget):
         self.canvas.clear()
         self.n += 1
         with self.canvas:
-            Color(1, 0, 0)
+            Color(0.0399625, 0.31640625, 0.4921875)
             for i in range(24):
                 x = 20 + 10 * i
                 y = 80 + 50 * cos(self.n * (self.ball_factors[i] / 30))
