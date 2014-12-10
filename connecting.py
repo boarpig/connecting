84fcdc4d87c15013391b6680393c2406d57c2c7b
Lauri Hakko <lauri.hakko@gmail.com>
2014-12-10 23:48:13 +0200

    resolved #1 made code more efficient

--- connecting.py
+++ connecting.py
@@ -16,17 +16,22 @@ class Animation(Widget):
     def __init__(self, **kwargs):
         super(Animation, self).__init__(**kwargs)
         self.ball_factors = list(linspace(1, 4, 24))
-        self.n = 0
-
-    def update(self, dt):
-        self.canvas.clear()
-        self.n += 1
+        self.balls = []
         with self.canvas:
             Color(0.0399625, 0.31640625, 0.4921875)
             for i in range(24):
                 x = 69 + 5 * i
-                y = 56 + 20 * cos(self.n * (self.ball_factors[i] / 30))
-                Ellipse(pos=(x, y), size=(4, 4))
+                y = 56
+                self.balls.append(Ellipse(pos=(x, y), size=(4, 4)))
+        self.bind(pos=self.update)
+        self.n = 0
+
+    def update(self, dt):
+        self.n += 1
+        for i in range(24):
+            x = 69 + 5 * i
+            y = 56 + 20 * cos(self.n * (self.ball_factors[i] / 30))
+            self.balls[i].pos = (x, y)
 
 
 class AnimationApp(App):
