fdc629c583b0edb5f0da8a9ac0213a1a33be5d4a
Lauri Hakko <lauri.hakko@gmail.com>
2014-12-11 11:34:51 +0200

    added the label

--- connecting.py
+++ connecting.py
@@ -6,7 +6,8 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.config import Config
 from kivy.graphics import Color, Ellipse
-from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 from math import cos
 from numpy import linspace
 
@@ -14,18 +15,23 @@ Config.set('graphics', 'width', '256')
 Config.set('graphics', 'height', '144')
 
 
-class Animation(Widget):
+class Animation(FloatLayout):
 
     def __init__(self, **kwargs):
         super(Animation, self).__init__(**kwargs)
         self.ball_factors = list(linspace(1, 4, 24))
         self.balls = []
+        self.label = ""
         with self.canvas:
             Color(0.04296875, 0.37109375, 0.58203125)
             for i in range(24):
                 x = 69 + 5 * i
                 y = 56
                 self.balls.append(Ellipse(pos=(x, y), size=(4, 4)))
+            self.label = Label(text="Connecting to Prismata...",
+                               color=(0.04296875, 0.37109375, 0.58203125, 1),
+                               pos=(75, 56), font_size='12pt',
+                               markup=True)
         self.bind(pos=self.update)
         self.n = 0
 
